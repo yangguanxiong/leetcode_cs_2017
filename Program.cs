95d2d7df9145a0e052ff36ecea0ec4ba7b665237
yangguanxiong <yang.grayson@gmail.com>
2017-07-04 11:04:10 -0400

    trival

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace leetcode_cs_2017
 {
@@ -7,6 +8,16 @@ namespace leetcode_cs_2017
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var dict = new Dictionary<string, int>();
+            var linkedList = new LinkedList<string>();
+            var set = new HashSet<int>();
+            var str = "abcea";
+            System.Console.WriteLine(str[2]);
+            System.Console.WriteLine(str.LastIndexOf('a')); 
+
+            // var queue = new Queue<int>();
+            // queue.
+              
         }
     }
 }
