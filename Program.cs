10df6e19ff0be74cfb762b6f49a2716b34409b6f
yangguanxiong <yang.grayson@gmail.com>
2017-07-04 10:43:48 -0400

    initialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcode_cs_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
