0e3740d70f426dd247fbe4119b3d1b32ab8e3911
yangguanxiong <yang.grayson@gmail.com>
2017-07-20 09:56:40 -0400

    clean up

--- Program.cs
+++ Program.cs
@@ -6,48 +6,9 @@ namespace leetcode_cs_2017
 {
     class Program
     {
-
-        public static bool IsPalindrome(ListNode head)
-        {
-            if (head == null || head.next == null)
-            {
-                return true;
-            }
-
-            var r = head;
-            int k = 0;
-            while (r != null)
-            {
-                r = r.next;
-                k++;
-            }
-
-            int steps = (k + 1) / 2;
-            var i = head;
-            var j = head;
-            while (steps > 0)
-            {
-                j = j.next;
-                steps--;
-            }
-            while (j != null)
-            {
-                if (j.val != i.val)
-                {
-                    return false;
-                }
-                j = j.next;
-            }
-            return true;
-        }
-
-
-
-
         /* Driver program to test above functions */
         public static void Main(String[] args)
         {
-
             
         }
 
